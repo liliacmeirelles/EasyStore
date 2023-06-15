0bb910d4b52a1e85c369ca8d33a16f01d9889a78
Lilia Meirelles <liliacmeirelles@gmail.com>
2023-06-14 22:31:24 -0300

    First Commit

---
+++ app/src/main/java/br/com/easystore/app/configuration/Application.java
@@ -0,0 +1,8 @@
+package br.com.easystore.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.easystore")
+public class Application { }
